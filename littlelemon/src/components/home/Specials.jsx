3b0f9626bc1e56b2acb29035060e70c7ca9dd72d
JINSEH <e.p.gamerstv@gmail.com>
2025-05-26 11:10:28 +0800

    Completed Specials.jsx

--- littlelemon/src/components/home/Specials.jsx
+++ littlelemon/src/components/home/Specials.jsx
@@ -1,5 +1,15 @@
 import styles from "../../modules/specials.module.css";
-import { Card, Image } from "@chakra-ui/react";
+import { Card, Image, Stack, Flex, Text, Button } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleChevronLeft,
+  faCircleChevronRight,
+  faT,
+  faTruck,
+} from "@fortawesome/free-solid-svg-icons";
+import greekSalad from "../../images/greeksalad.png";
+import bruschetta from "../../images/bruschetta.png";
+import lemondessert from "../../images/lemondessert.png";
 
 export default function Specials() {
   return (
@@ -10,12 +20,86 @@ export default function Specials() {
           <button>Online Menu</button>
         </div>
       </section>
-      <section id={styles.cardContainer}>
-        <Card.Root bg="gray.200" borderWidth={0} size="md">
-          <Card.Header />
-          <Card.Body />
-          <Card.Footer />
-        </Card.Root>
+
+      <section className={styles.cardContainer}>
+        <div className={styles.arrows}>
+          <FontAwesomeIcon icon={faCircleChevronLeft} size="s" />
+          <FontAwesomeIcon icon={faCircleChevronRight} size="s" />
+        </div>
+
+        {/* Card Container */}
+        <Stack
+          width="60%"
+          justify={{ base: "center", lg: "space-between" }}
+          alignItems={"center"}
+          gap={2}
+          direction={{ base: "column", lg: "row" }}
+        >
+          {/* First Card */}
+          <Card.Root w={"100%"} h={"100%"} border={"none"} bg={"gray.200"}>
+            <Image bg={"white"} h={"100%"} src={greekSalad} alt="Greek salad" />
+            <Card.Body gap="2">
+              <Flex justify={"space-between"}>
+                <Card.Title color={"black"}>Greek salad</Card.Title>
+                <Text color={"orange"}>$12.99</Text>
+              </Flex>
+              <Card.Description>
+                The famous greek salad of crispy lettuce, peppers, olives and
+                our Chicago style feta cheese, garnished with crunchy garlic and
+                rosemary croutons.
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer gap="2">
+              <Button variant={"plain"} color={"black"} padding={"0"}>
+                Order a delivery
+                <FontAwesomeIcon icon={faTruck} />
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+
+          {/* Second Card */}
+          <Card.Root w={"100%"} h={"100%"} border={"none"} bg={"gray.200"}>
+            <Image bg={"white"} src={bruschetta} />
+            <Card.Body gap="3">
+              <Flex justify={"space-between"}>
+                <Card.Title color={"black"}>Bruschetta</Card.Title>
+                <Text color={"orange"}>$5.99</Text>
+              </Flex>
+              <Card.Description>
+                Our Bruschetta is made from grilled bread that has been smeared
+                with garlic and seasoned with salt and olive oil.
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer gap="3">
+              <Button variant={"plain"} color={"black"} padding={"0"}>
+                Order a delivery
+                <FontAwesomeIcon icon={faTruck} />
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+
+          {/* Third Card */}
+          <Card.Root w={"100%"} h={"100%"} border={"none"} bg={"gray.200"}>
+            <Image bg={"white"} src={lemondessert} />
+            <Card.Body gap="3">
+              <Flex justify={"space-between"}>
+                <Card.Title color={"black"}>Lemon Dessert</Card.Title>
+                <Text color={"orange"}>$5.00</Text>
+              </Flex>
+              <Card.Description>
+                This comes straight from grandma’s recipe book, every last
+                ingredient has been sourced and is as authentic as can be
+                imagined.
+              </Card.Description>
+            </Card.Body>
+            <Card.Footer gap="2">
+              <Button variant={"plain"} color={"black"} padding={"0"}>
+                Order a delivery
+                <FontAwesomeIcon icon={faTruck} />
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </Stack>
       </section>
     </>
   );
